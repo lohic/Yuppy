421794786ae1bab396d9a20b69f96ece2f831b99
Loïc Horellou <loic@formidable-studio.net>
2014-05-14 23:17:24 +0200

    what' sound addClass .hover

--- js/script.js
+++ js/script.js
@@ -16,12 +16,24 @@ jQuery( document ).ready(function( $ ) {
 	$('#carousel-home').on('slide.bs.carousel', function () {
 	  // do something…
 	  //$('#carousel-home').carousel('pause');
-	  console.log("pret à slider");
+	  // console.log("pret à slider");
 	});
 
 	$('#carousel-home').on('slid.bs.carousel', function () {
 	  // do something…
-	  console.log("fin du slide");
+	  // console.log("fin du slide");
+	});
+
+	$('.whatsound-bloc .vignette').on('mouseenter mouseleave',function(event){
+		//console.log(event.type);
+		if(event.type == 'mouseenter'){
+			$(this).parent().parent().find('h3 a').addClass('hover');
+		}
+		if(event.type == 'mouseleave'){
+			$(this).parent().parent().find('h3 a').removeClass('hover');
+		}
+
+		// console.log($(this).parent().parent().find('h3 a').text());
 	});
 
 });
