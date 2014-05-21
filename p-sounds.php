6d5fd961a6ce7d93b7382d8f720abaa6dc17f06a
Loïc Horellou <loic@formidable-studio.net>
2014-05-21 17:04:54 +0200

    ajout player sound cloud dans page sounds

--- p-sounds.php
+++ p-sounds.php
@@ -11,6 +11,10 @@ Template Name: Discographie
 
 <div id="content" class="row">
 
+	<div class="record col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<iframe class="iframe" width="100%" height="465" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/beyuppy&amp;auto_play=false&amp;auto_advance=true&amp;buying=true&amp;liking=true&amp;download=true&amp;sharing=true&amp;show_artwork=true&amp;show_comments=true&amp;show_playcount=true&amp;show_user=true&amp;hide_related=true&amp;visual=false&amp;start_track=0&amp;callback=true&amp;color=00b5ff">
+		  </iframe>
+	</div>
 
 	<?php if(have_posts()) : ?>
 		<div class="record col-lg-12 col-md-12 col-sm-12 col-xs-12">
