23f7653a6d024b3df7b061cb00a438cf79d81855
Sophie Blum <so@iMac-de-Sophie-Blum.local>
2014-05-27 16:20:52 +0200

    Interviews

--- single.php
+++ single.php
@@ -7,12 +7,11 @@
 		
 <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
 		<div class="post" id="post-<?php the_ID(); ?>">
-				<h1 class="page-header"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
-				
-				<p class="postmetadata">
-					<small><?php the_date() ?> à <?php the_time() ?> par <?php the_author() ?> | Cat&eacute;gorie : <?php the_category(', ') ?> | <?php the_tags() ?></small>
-				</p>
-				
+				<h1 class="page-header"><?php the_title(); ?></h1>
+
+				<p class="postmetadata"><?php the_category(', ') ?></p>
+				<p class="postmetadata"><?php the_date() ?><br/><span class="tags"><?php the_tags() ?></span></p>
+
 				<div class="post_content">
 					<?php the_content(); ?>
 				</div>
@@ -24,7 +23,7 @@
 	
 	<?php endwhile; ?>
 	<?php else : ?>
-			<p>Désolé, aucun article ne correspond à vos critères.</p>
+			<p>Désolé, rien de tel ici.</p>
 <?php endif; ?>
 	</div>
 </div>
