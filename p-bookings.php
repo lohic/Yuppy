1caa8e270e28858b6759c98ec9c6939342adc267
Sophie Blum <so@iMac-de-Sophie-Blum.local>
2014-06-10 10:56:57 +0200

    Pb class : record > records

--- p-bookings.php
+++ p-bookings.php
@@ -13,7 +13,7 @@ Template Name: Bookings
 
 
 	<?php if(have_posts()) : ?>
-		<div class="record col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div class="records col-lg-12 col-md-12 col-sm-12 col-xs-12">
 		<?php while(have_posts()) : the_post(); ?>
 			<div class="post" id="post-<?php the_ID(); ?>">
 				<h1 class="page-header"><?php the_title(); ?></h1>				
@@ -25,7 +25,7 @@ Template Name: Bookings
 		</div>
 
 
-		<div class="record col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div class="records col-lg-12 col-md-12 col-sm-12 col-xs-12">
 			<div class="row">
 				<?php
 				$custom_posts = new WP_Query('post_type=groupe&posts_per_page=-1');
@@ -33,7 +33,7 @@ Template Name: Bookings
 					$i = 0;
 				?>
 				    <?php while ($custom_posts->have_posts()): $custom_posts->the_post(); ?>
-				        <div class="record col-lg-4 col-md-4 col-sm-6 col-xs-6">
+				        <div class="records col-lg-4 col-md-4 col-sm-6 col-xs-6">
 				        	<?php if ( has_post_thumbnail() )?><a href="<?php the_permalink(); ?>"><?php { the_post_thumbnail('home', array('class' => 'img-responsive'));?></a><?php } ?>
 				            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 				        </div>
