c13da40d153aa5d36ac1126f8260e60072f914e3
Loïc Horellou <loic@formidable-studio.net>
2014-06-18 15:47:43 +0200

    update excerpt

--- functions.php
+++ functions.php
@@ -571,3 +571,45 @@ add_shortcode( 'aside', 'aside_shortcode' );
 
 
 
+/******************************************************************************
+* @Author: Boutros AbiChedid 
+* @Date:   June 20, 2011
+* @Websites: http://bacsoftwareconsulting.com/ ; http://blueoliveonline.com/
+* @Description: Preserves HTML formating to the automatically generated Excerpt.
+* Also Code modifies the default excerpt_length and excerpt_more filters.
+* @Tested: Up to WordPress version 3.1.3
+*******************************************************************************/
+function custom_wp_trim_excerpt($text) {
+$raw_excerpt = $text;
+if ( '' == $text ) {
+    //Retrieve the post content. 
+    $text = get_the_content('');
+ 
+    //Delete all shortcode tags from the content. 
+    $text = strip_shortcodes( $text );
+ 
+    $text = apply_filters('the_content', $text);
+    $text = str_replace(']]>', ']]&gt;', $text);
+     
+    $allowed_tags = '<a><br><p>'; /*** MODIFY THIS. Add the allowed HTML tags separated by a comma.***/
+    $text = strip_tags($text, $allowed_tags);
+     
+    $excerpt_word_count = 55; /*** MODIFY THIS. change the excerpt word count to any integer you like.***/
+    $excerpt_length = apply_filters('excerpt_length', $excerpt_word_count); 
+     
+    $excerpt_end = '[...]'; /*** MODIFY THIS. change the excerpt endind to something else.***/
+    $excerpt_more = apply_filters('excerpt_more', ' ' . $excerpt_end);
+     
+    $words = preg_split("/[\n\r\t ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY);
+    if ( count($words) > $excerpt_length ) {
+        array_pop($words);
+        $text = implode(' ', $words);
+        $text = $text . $excerpt_more;
+    } else {
+        $text = implode(' ', $words);
+    }
+}
+return apply_filters('wp_trim_excerpt', $text, $raw_excerpt);
+}
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+add_filter('get_the_excerpt', 'custom_wp_trim_excerpt');
